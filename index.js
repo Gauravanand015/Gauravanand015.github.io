2f2532380d8ce4a819505f59302a9701ab9da35e
Gaurav Anand <gaurav56anand@gmail.com>
2023-02-15 00:38:36 +0530

    Update portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+const navResume = document.getElementById("resume-button-1");
+navResume.addEventListener("click", () => {
+  window.location =
+    "https://drive.google.com/uc?export=download&id=1-Nhtkl7bYoi5G7333vplub43AD0BS3eh";
+});
+
+
+const navResume2 = document.getElementById("resume-button-2");
+navResume2.addEventListener("click", () => {
+  window.location =
+    "https://drive.google.com/uc?export=download&id=1-Nhtkl7bYoi5G7333vplub43AD0BS3eh";
+});
